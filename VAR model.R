505d91ff5d07e8e3da42329346c86abb7e28489a
malepati rahul <malepatirahul@hotmail.com>
2021-01-28 09:16:58 +0000

    trend commit

--- VAR model.R
+++ VAR model.R
@@ -207,7 +207,7 @@ lagselect$selection
 
 # Building VAR model 
 
-model                      <- VAR(tf , p = 3 , type = 'const' , season = NULL , exogen = NULL)
+model                      <- VAR(tf , p = 3 , type = 'trend' , season = NULL , exogen = NULL)
 summary(model)
 
 # Daignosing VAR model
